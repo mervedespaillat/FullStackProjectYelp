4c2e91562842ed8b0236c77949d33fcc306d3647
Merve Dogan-Espaillat <1mervedogan@gmail.com>
2023-06-08 14:34:14 -0400

    show page updates

--- app/views/api/users/show.json.jbuilder
+++ app/views/api/users/show.json.jbuilder
@@ -1,5 +1,4 @@
 json.user do
     json.extract! @user, :id, :first_name, :created_at
     json.photo @user.photo.attached? ? url_for(@user.photo) : nil
-
 end
--- frontend/src/components/Map/index.js
+++ frontend/src/components/Map/index.js
@@ -1,27 +1,13 @@
 import { Wrapper } from "@googlemaps/react-wrapper";
 import { useEffect, useRef, useState } from "react";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import './map.css'
-import { getReview } from "../../store/reviews";
 
 // default
-
-const MeltMapWrapper=({shopId})=> {
-  
-  // const { shopId } = useParams();
-    // const shop = useSelector(getReview(shopId));
-    // const lat = shop.latitude;
-    // const lng = shop.longitude;
-    let key = process.env.GOOGLE_MAPS_API_KEY
-    const center =  { lat: 40.7184296, lng: 0 };
-    const zoom = 15;
-
-
+const MeltMapWrapper=()=> {
     return (
         <div className="map">
-       <Wrapper apiKey={key}>
-         <MyMapComponent center={center} zoom={zoom} />
+       <Wrapper apiKey={"AIzaSyAFxMaLXHUS9fPBdsU_8mT25fi_mdLkYxo"}>
+         <MyMapComponent center={{ lat: 40.932690, lng: 29.126810 }} zoom={10} />
        </Wrapper>
          </div>
     )
@@ -37,7 +23,7 @@ export function MyMapComponent({ center, zoom }) {
     });
   });
 
-  return <div ref={ref}  style={{ width: '300px', height: '300px' }}  className="map" />;
+  return <div ref={ref} className="map" />;
 }
 
 export default MeltMapWrapper
