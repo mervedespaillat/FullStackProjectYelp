4e5858eff0b8ba4e9bff10baf96e8111cfe2cd76
Merve Dogan-Espaillat <1mervedogan@gmail.com>
2023-08-02 12:49:12 -0400

    css

--- frontend/src/components/Reviews/reviewIndexItem.js
+++ frontend/src/components/Reviews/reviewIndexItem.js
@@ -1,38 +1,59 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteReview } from '../../store/reviews';
-// import { formatDate } from '../util/dateUtil';
-import "./reviewIndex.css"
-import RatingStars from '../RatingStars/ratingStars';
-import { formatDateTime } from '../../util/date';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { deleteReview } from "../../store/reviews";
+import "./reviewIndex.css";
+import RatingStars from "../RatingStars/ratingStars";
+import { formatDateTime } from "../../util/date";
 
-const ReviewIndexItem = ({review}) => {
-    const dispatch = useDispatch()
+const ReviewIndexItem = ({ review }) => {
+  const dispatch = useDispatch();
 
-    // const date = formatDate(review.createdAt)
-    const [rating, setRating] = useState(review.rating)
-    const handleClick = () =>{
-        
-        dispatch(deleteReview(review.id))
-    }
-    if(!review) return null
+  const [rating, setRating] = useState(review.rating);
+  const handleClick = () => {
+    dispatch(deleteReview(review.id));
+  };
 
-    const date = formatDateTime(review.createdAt) 
-    return (
-        <div className='review-container'>
-        <div className='user-details'>
-            <div className='profile-photo'><img src={review.userPhoto} alt="" className='user-photo'/></div>
-            <div className='profile-name'>{review.userFname} {review.userLname}</div>
-            <div className='delete-btn-review'><button onClick={handleClick}>Delete</button></div>
+  if (!review) return null;
+
+  const showUserIcon = !review.userPhoto || review.userPhoto.trim() === "";
+
+  const date = formatDateTime(review.createdAt);
+
+  return (
+    <div className="review-container">
+      <div className="user-details">
+        {showUserIcon ? (
+          <div className="profile-photo">
+            {/* <i className="fa-solid fa-user"></i> */}
+            <img
+              src="https://img.freepik.com/premium-vector/flat-instagram-icons-notifications_619991-50.jpg?w=1380"
+              alt=""
+              className="user-photo userNull"
+            ></img>
+          </div>
+        ) : (
+          <div className="profile-photo">
+            <img src={review.userPhoto} alt="" className="user-photo" />
+          </div>
+        )}
+        <div className="profile-name">
+          {review.userFname} {review.userLname}
         </div>
-        <div className='rating-container'> <RatingStars rating={review.rating} setRating={setRating} readOnly={true}/>
-        <p>{date}</p></div>
-        <div className='rating-context'>
-            {review.body}</div>
+        <div className="delete-btn-review">
+          <button onClick={handleClick}>Delete</button>
         </div>
-    )
+      </div>
+      <div className="rating-container">
+        <RatingStars
+          rating={review.rating}
+          setRating={setRating}
+          readOnly={true}
+        />
+        <p>{date}</p>
+      </div>
+      <div className="rating-context">{review.body}</div>
+    </div>
+  );
+};
 
-}
-
-export default ReviewIndexItem
+export default ReviewIndexItem;
